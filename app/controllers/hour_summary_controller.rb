d28dd693b08450b9d8b329a04632d4d106a7c04e
Kevin Marsh <kevin.marsh@gmail.com>
2010-07-29 16:41:20 -0400

    Hours

---
+++ app/controllers/hour_summary_controller.rb
@@ -0,0 +1,12 @@
+class HourSummaryController < ApplicationController
+  unloadable
+
+  def index
+    @hours_this_week = TimeEntry.sum(:hours, :conditions => ['user_id = ? AND YEARWEEK(spent_on, 1) = YEARWEEK(NOW(), 1)', 1])
+    @hours_to_go = 30.0 - @hours_this_week
+    @average_per_day = @hours_this_week / Date.today.wday
+    
+    render :layout => false
+  end
+  
+end
